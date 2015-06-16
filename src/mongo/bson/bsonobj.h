c2fdab7e0e859a8a9201390ef32c33bc0aff5512
Raymond Jacobson <raymond.jacobson@10gen.com>
2015-06-16 09:59:05 -0400

    wip

--- src/mongo/bson/bsonobj.h
+++ src/mongo/bson/bsonobj.h
@@ -78,6 +78,7 @@ namespace mongo {
      OID:       an OID object
      NumberDouble: <double>
      NumberInt: <int32>
+     NumberDecimal: <dec128>
      String:    <unsigned32 strsizewithnull><cstring>
      Date:      <8bytes>
      Regex:     <cstring regex><cstring options>
