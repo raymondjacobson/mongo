860b1cb3f97881e479a00f2bf40bccaf80521e63
Raymond Jacobson <raymond.jacobson@10gen.com>
2015-07-29 09:23:58 -0400

    !fixup Fix typo bug for shim_intel_decimal128.cpp

--- src/third_party/shim_intel_decimal128.cpp
+++ src/third_party/shim_intel_decimal128.cpp
@@ -1,3 +1,3 @@
-// This file intentionally blank.  shim_IntelRDFPMathLib20U1.cpp is part of the
+// This file intentionally blank.  shim_intel_decimal128.cpp is part of the
 // third_party/IntelRDFPMathLib20U1 library, which is just a placeholder for forwarding
 // library dependencies.
