c7a06db42ad985269f9c25d39f4305da222a0927
Raymond Jacobson <raymond.jacobson@10gen.com>
2015-07-30 09:36:03 -0400

    Add demo

---
+++ demo/decimal.js
@@ -0,0 +1,31 @@
+db.decimalBank.drop();
+
+var initialBalance = NumberDecimal("2000000000");
+var toDong = NumberDecimal("21819.00");
+var fromDong = NumberDecimal("0.000046");
+
+db.decimalBank.insert({
+	balance : initialBalance 
+});
+
+Math.seed = 6;
+Math.seededRandom = function(max, min) {
+    max = max || 1;
+    min = min || 0;
+ 
+    Math.seed = (Math.seed * 9301 + 49297) % 233280;
+    var rnd = Math.seed / 233280;
+ 
+    return min + rnd * (max - min);
+}
+
+var iterations = 10000;
+for (var i = 0; i < iterations; ++i) {
+	var transactionAmount = Math.seededRandom()*100;
+	var direction = (Math.seededRandom() > 0.5 ? 1 : -1);
+	transactionAmount *= direction;
+
+	db.decimalBank.update({}, {$mul : {balance : toDong} });
+	db.decimalBank.update({}, {$inc : {balance : transactionAmount} });
+	db.decimalBank.update({}, {$mul : {balance : fromDong} });
+}
---
+++ demo/double.js
@@ -0,0 +1,31 @@
+db.doubleBank.drop();
+
+var initialBalance = 2000000000;
+var toDong = 21819.00;
+var fromDong = 0.000046;
+
+db.doubleBank.insert({
+	balance : initialBalance 
+});
+
+Math.seed = 6;
+Math.seededRandom = function(max, min) {
+    max = max || 1;
+    min = min || 0;
+ 
+    Math.seed = (Math.seed * 9301 + 49297) % 233280;
+    var rnd = Math.seed / 233280;
+ 
+    return min + rnd * (max - min);
+}
+
+var iterations = 10000;
+for (var i = 0; i < iterations; ++i) {
+	var transactionAmount = Math.seededRandom()*1000000;
+	var direction = (Math.seededRandom() > 0.5 ? 1 : -1);
+	transactionAmount *= direction;
+
+	db.doubleBank.update({}, {$mul : {balance : toDong} });
+	db.doubleBank.update({}, {$inc : {balance : transactionAmount} });
+	db.doubleBank.update({}, {$mul : {balance : fromDong} });
+}
